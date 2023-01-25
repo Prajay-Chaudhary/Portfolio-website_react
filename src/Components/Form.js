03c1329d1b1d02a7cbb1ad8c82e7b4584f2b0d5a
Prajay Chaudhary <prajaychaudhary5678@gmail.com>
2023-01-25 17:36:19 +0100

    Made contact form to send email using emailJS

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -1,19 +1,36 @@
 import "./FormStyles.css";
-import React from 'react'
+import React, { useRef } from 'react'
+import emailjs from "@emailjs/browser";
 
 const Form = () => {
+  const form = useRef();
+  const sendEmail = (e) => {
+    e.preventDefault();
+    emailjs.sendForm(
+      "service_l9j07fh",
+      "template_xrsf87o",
+      form.current,
+      'cf45PitOTu4s5g5mJ')
+      .then((result) => {
+          console.log(result.text);
+          alert("Form submitted successfully!");
+      }, (error) => {
+          console.log(error.text);
+          alert("FAILED...", error);
+      });
+  };
   return (
     <div className="form">
-      <form>
+      <form ref={form} onSubmit={sendEmail}>
         <label>Your Name</label>
-        <input type="text" required></input>
+        <input type="text" name="user_name" required></input>
         <label>Your Email</label>
-        <input type="email" required></input>
+        <input type="email" name="user_email" required></input>
         <label>Subject</label>
-        <input type="text" required></input>
+        <input type="text" name="user_subject" required></input>
         <label>Message</label>
-        <textarea rows="6" placeholder="Type your message here..." required />
-        <button className="btn">Submit</button>
+        <textarea rows="6" placeholder="Type your message here..." name="user_message" required />
+        <button className="btn" type="submit" value="Send">Submit</button>
       </form>
     </div>
   );
