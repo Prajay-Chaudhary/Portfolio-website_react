2cba0b7c27189bb7eefc5a724486220be1e0789d
Prajay Chaudhary <prajaychaudhary5678@gmail.com>
2023-08-23 22:00:00 +0200

    Maintained about page's about content, cv and image.

--- src/Components/AboutContent.js
+++ src/Components/AboutContent.js
@@ -8,7 +8,7 @@ import CV from "../assets/CV.jpg"
 class AboutContent extends Component {
   pdfGenerate = () => {
     var doc = new jsPDF("portrait", "px", "a4", "false");
-    doc.addImage(CV, "jpg", -5, 0, 450, 500);
+    doc.addImage(CV, "jpg", 0, 0, 450, 500);
     doc.save("CV.pdf");
   }
   render() {
@@ -23,25 +23,28 @@ class AboutContent extends Component {
               <h1 className="about__heading">About Me</h1>
               <div className="about_meta">
                 <p className="about_text ">
-                  I am a full-stack developer with skills in Ruby on Rails and React JS.
-                  Having a passion for building beautiful, intuitive, and functional websites and
-                  applications.
-                  I am able to create dynamic, full-featured web
-                  applications that provide a seamless user experience.
+                  I am currently pursuing a Master's degree at Sup De Vinci College,
+                  with a strong aspiration to engage in a work-study program as a Full Stack Developer.
+                  My preferred schedule involves alternating between 1 week of intensive learning and 3 weeks of practical application within a business setting.
+                  I am committed to making myself available whenever possible, striving to make valuable contributions while ensuring continuous learning and growth.
                 </p>
                 <p className="about_text ">
-                  As a problem solver who loves facing challenges in my work,
-                  I'm excited to be at the deployment phase of my career as a
-                  full-stack developer. I'm passionate and ambitious about my
-                  work, and love to be on a team that questions possibilities.
-                  I've previously been a Web Developer, and bring those skills to my
-                  web application development on a team.
+                  I identify myself as a proficient full-stack developer, specializing in Ruby on Rails, React JS, and Python.
+                  My expertise lies in crafting elegant, user-centric websites and applications that seamlessly blend form and function. With a fervent enthusiasm for creating dynamic, feature-rich web solutions,
+                  I am dedicated to delivering a cohesive and enjoyable user experience.
                 </p>
                 <p className="about_text ">
-                  A leadership driven worker, Prajay Chaudhary enjoys working
-                  both on his own and as part of a team to work toward
-                  challenging goals. He has built numerous web applications and database
-                  systems.
+                  Driven by a passion for problem-solving, I eagerly embrace challenges in my professional journey.
+                  I am particularly thrilled to be entering the deployment phase of my career as a full-stack developer, poised to bring innovative solutions to fruition. My ambition fuels my commitment to pushing boundaries and exploring the realms of possibility. Having previously excelled as a Web Developer, I bring a wealth of experience to the realm of web application development,
+                  enriching collaborative efforts within a team.
+                </p>
+                <p className="about_text ">
+                  Recognized for my leadership-oriented approach, I find satisfaction in both independent work and collaborative endeavors, consistently striving to exceed ambitious objectives. I am proud to have spearheaded the development of numerous web applications and database systems, showcasing my proficiency in turning concepts into reality.
+
+                  Thank you for considering my profile.
+
+                  Sincerely,
+                  Prajay Chaudhary
                 </p>
                 <div className="about_btn">
                   <a href="#">
